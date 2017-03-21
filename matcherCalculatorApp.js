dca846829f7287bd76a447a00345f665257b38e7
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-03-20 21:42:48 -0700

    Winston logging library for Node.js

--- matcherCalculatorApp.js
+++ matcherCalculatorApp.js
@@ -1,10 +1,12 @@
 /**
  * Created by yashaswithannir on 3/19/17.
  */
+var logger = require('./log');
 describe("Testing the calculator Application", function(){
     var title;
     beforeEach(function () {
         browser.get("http://www.way2automation.com/angularjs-protractor/calc/");
+        logger.log('info','Navigating to the website');
         title = browser.getTitle();
     });
     afterEach(function () {
@@ -13,16 +15,19 @@ describe("Testing the calculator Application", function(){
     it("Title match", function(){
         title.then(function(text){
             expect(text).toEqual("Protractor practice website - Calculator");
+            logger.log('info','validating the title');
         });
     });
     it("Title do not match", function(){
         title.then(function(text){
             expect(text).not.toEqual("Prfdsfdotracdasdtor practice website - Calculator");
+            logger.log('info','validating the title to be wrong');
         });
     });
     it("Title matches partially", function(){
         title.then(function(text){
             expect(text).toMatch("website - Calculator");
+            logger.log('info','validating the title to match partially');
         });
     });
 });
