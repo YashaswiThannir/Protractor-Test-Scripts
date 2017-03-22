c091f3774ec6a9ff70bcf5ed6ab1ce52439740af
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-03-22 01:31:54 -0700

    Jasmine-data-provider included for multiple users

--- gmailTest.js
+++ gmailTest.js
@@ -3,6 +3,7 @@
  */
 
 var objects = require('./objects.json');
+var use = require('jasmine-data-provider');
 
 describe("Gmail Login Test", function(){
    beforeEach(function(){
@@ -11,10 +12,21 @@ describe("Gmail Login Test", function(){
        browser.get(objects.siteurl);
        console.log("The site url is: "+objects.siteurl);
        browser.sleep(4000);
+   });
+   function providerData(){
+       return[
+           {email:objects.userdetails.email1, password:objects.userdetails.password1},
+           {email:objects.userdetails.email2, password:objects.userdetails.password2},
+       ];
+   }
+   use(providerData, function(data){
+       it("Validating user credentials", function(){
+           element(by.id(objects.locators.loginpage.email)).sendKeys(data.email);
+           console.log(data.password);
+           element(by.id(objects.locators.loginpage.button)).click();
+           browser.get(objects.siteurl);
 
+       });
    });
-   it("Validating user credentials", function(){
-       element(by.id(objects.locators.loginpage.email)).sendKeys(objects.userdetails.email);
-       element(by.id(objects.locators.loginpage.button)).click();
-   });
+
 });
