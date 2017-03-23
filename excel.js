aca787b7e629d0030bd19ff1b2f6d78e3967af47
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-03-22 20:30:48 -0700

    Protractor Script to read data from Excel Sheet

---
+++ excel.js
@@ -0,0 +1,23 @@
+/**
+ * Created by yashaswithannir on 3/22/17.
+ */
+
+var XLSX = require('xlsx');
+
+var workbook = XLSX.readFile('test.xlsx');
+
+var first_sheet_name = workbook.SheetNames[0];
+
+//var first_sheet_name = "LoginTest";
+
+var address_of_cell = 'B2';
+
+var worksheet = workbook.Sheets[first_sheet_name];
+
+/* Find desired cell */
+var desired_cell = worksheet[address_of_cell];
+
+/* Get the value */
+var desired_value = desired_cell.v;
+
+console.log(desired_value);
