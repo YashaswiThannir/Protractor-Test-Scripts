2c1b96d1f502e1f1f3fae3c1a91f0883f04be00f
Yashaswi Thannir <YashaswiThannir@users.noreply.github.com>
2017-03-22 20:47:48 -0700

    Add files via upload

--- excel.js
+++ excel.js
@@ -10,7 +10,7 @@ var first_sheet_name = workbook.SheetNames[0];
 
 //var first_sheet_name = "LoginTest";
 
-var address_of_cell = 'B2';
+var address_of_cell = 'A2';
 
 var worksheet = workbook.Sheets[first_sheet_name];
 
@@ -20,4 +20,10 @@ var desired_cell = worksheet[address_of_cell];
 /* Get the value */
 var desired_value = desired_cell.v;
 
-console.log(desired_value);
+describe("Testing excel data usage", function(){
+    it("validating excel data", function(){
+        browser.get("http://www.way2automation.com/angularjs-protractor/registeration/#/login");
+        element(by.id("username")).sendKeys(desired_value);
+        browser.sleep(2000);
+    });
+} );
